c4545a02414b648b1d3f25a42f798f5472c4eb47
Austin Hanni-Wells <a-hanni-wells@onu.edu>
2023-04-04 08:07:26 -0400

    changed from Enum back to name

--- src/main/java/Effect.java
+++ src/main/java/Effect.java
@@ -1,19 +1,19 @@
 import java.util.Objects;
 
 public class Effect {
-    public int Sickness(String Enum, String type, int health) {
+    public int Sickness(String name, String type, int health) {
         if (health <= 20) {
-            System.out.println(Enum + " has died.");
+            System.out.println(name + " has died.");
             health = 0;
             return health;
         }
         else if (Objects.equals(type, "measles")) {
-            System.out.println(Enum + " got measles.");
+            System.out.println(name + " got measles.");
             health = health - 20;
             return health;
         }
         else {
-            System.out.println(Enum + " got dysentery.");
+            System.out.println(name + " got dysentery.");
             health = health - 20;
             return health;
         }
